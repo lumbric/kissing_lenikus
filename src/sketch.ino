6c2a0c4caa55182d2ff12f2c09ecb41d6f43d4ec
lumbric <lumbric@gmail.com>
2015-04-15 17:41:15 +0200

    longer pause between as suggested by esel

--- src/sketch.ino
+++ src/sketch.ino
@@ -10,10 +10,10 @@
 
 // Auta mporeis na alakseis,  kai meta patas upload
 int SERVO_POS_BACK             = 15;          // a value between 0 and 180
-int SERVO_POS_FRONT            = 75;         // must be greater than SERVO_POS_BACK
+int SERVO_POS_FRONT            = 75;          // must be greater than SERVO_POS_BACK
 int SERVO_DELAY                = 3;           // increase to make servo slower, in ms
 int KISS_LENGTH                = 0.8 * 1000;  // delay front, in millisecs
-int PAUSE_BETWEEN              = 2 * 1000;    // pause between two kisses
+unsigned long PAUSE_BETWEEN    = 17 * 1000UL; // pause between two kisses
 int NUMBER_KISSES              = 3;           // used only for manual trigger via button
 unsigned long SERVO_IDLE_MS    = 7000L;       // servo goes idle after X seconds
                                               // this might increase lifetime
